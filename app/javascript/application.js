159044efe3332975364bd6497f488ca7415acf11
johnfinley <johnfinleybremerton@gmail.com>
2022-11-15 16:50:13 -0700

    can only vote on posts if logged in

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,22 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "bootstrap";
 
+
+
 $(function () {
   $(".vote").on("click", ".upvote, .downvote", function () {
     let post_id = $(this).parent().attr("data-id");
-    let is_upvote = $(this).hasClass("upvote")
+    let is_upvote = $(this).hasClass("upvote");
+    let signed_in = $(this).parent().attr("data-account")
+    
+    if (signed_in === "true" ) {
+      $.ajax({
+        url: "/posts/vote",
+        method: "POST",
+        data: { post_id: post_id, upvote: is_upvote },
+      });
+    } else if (signed_in === "false") {
+      alert("Please login or signup to vote on posts!")
+    }
 
-    $.ajax({
-      url: "/posts/vote",
-      method: "POST",
-      data: { post_id: post_id, upvote: is_upvote },
-    });
   });
 });
-
-// $(function () {
-//   $(".vote").on("click", ".downvote", function () {
-//     let post_id = $(this).parent().attr("data-id");
-//     console.log("downvote " + post_id);
-//   });
-// });
