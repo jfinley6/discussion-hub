38c21164df6916071410e513f319ad41a470fa59
johnfinley <johnfinleybremerton@gmail.com>
2022-11-15 20:29:17 -0700

    changed default in two columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_041526) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_022636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "username"
+    t.string "username", default: "", null: false
     t.text "bio"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_041526) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "karma"
+    t.integer "karma", default: 0
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
