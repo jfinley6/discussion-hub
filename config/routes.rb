9f209bbe1c9cde1cd05b446ea75f5dcf4dd16005
johnfinley <johnfinleybremerton@gmail.com>
2022-11-16 13:39:40 -0700

    added :destroy to comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :subscriptions, except: :destroy
   delete '/subscriptions/:community_id', to: 'subscriptions#destroy'
 
-  resources :comments, only: [:create]
+  resources :comments, only: [:create, :destroy]
 
   post '/posts/vote', to: 'votes#create' 
 
