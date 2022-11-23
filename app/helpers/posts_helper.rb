e31c1612ff96810098451998ffde04ba53b56851
johnfinley <johnfinleybremerton@gmail.com>
2022-11-22 17:15:11 -0700

    additional conditions for whether user is signed in

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,13 @@
 module PostsHelper
 
-    def postSortSelection(url, id)
+    def postSortSelection(url, id, sort)
         if url === "http://localhost:3000/new" && id === "new"
             "override"
-        elsif url === "http://localhost:3000/" && id === "top" 
+        elsif url === "http://localhost:3000/" && id === "front_page" && sort === "front_page" 
             "override"
-        elsif url === "http://localhost:3000/front_page" && id === "front_page" 
+        elsif url === "http://localhost:3000/top" && id === "top" && sort === "top"
+            "override"
+        elsif url === "http://localhost:3000/" && id === "top" && sort === "top"
             "override"
         end
     end
