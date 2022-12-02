5e43594b9e15f1260a18fc410465a8322ce7fbf4
johnfinley <johnfinleybremerton@gmail.com>
2022-12-02 16:03:36 -0700

    community url now must be unique

--- app/models/community.rb
+++ app/models/community.rb
@@ -3,6 +3,7 @@ class Community < ActiveRecord::Base
    friendly_id :url, use: [:slugged, :finders]  
    belongs_to :account
    validates_presence_of :url, :rules, :summary
+   validates :url, uniqueness: true
    has_many :posts
    has_many :subscriptions
    has_many :subscribers, through: :subscriptions, source: :account
