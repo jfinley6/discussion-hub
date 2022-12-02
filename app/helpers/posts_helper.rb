a429501d2ae93c27660d18ddf374a8d228b2ec9a
johnfinley <johnfinleybremerton@gmail.com>
2022-12-02 15:05:38 -0700

    changed localhost url to heroku url

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,13 @@
 module PostsHelper
 
     def postSortSelection(url, id, sort)
-        if url === "http://localhost:3000/new" && id === "new"
+        if url === "https://reddit-clone-rails.herokuapp.com/new" && id === "new"
             "override"
-        elsif url === "http://localhost:3000/" && id === "front_page" && sort === "front_page" 
+        elsif url === "https://reddit-clone-rails.herokuapp.com/" && id === "front_page" && sort === "front_page" 
             "override"
-        elsif url === "http://localhost:3000/top" && id === "top" && sort === "top"
+        elsif url === "https://reddit-clone-rails.herokuapp.com/top" && id === "top" && sort === "top"
             "override"
-        elsif url === "http://localhost:3000/" && id === "top" && sort === "top"
+        elsif url === "https://reddit-clone-rails.herokuapp.com/" && id === "top" && sort === "top"
             "override"
         end
     end
