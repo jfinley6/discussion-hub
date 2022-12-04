5ccacd17d8a135e5ffea4117275d03a8532a0e1e
johnfinley <johnfinleybremerton@gmail.com>
2022-12-04 09:47:25 -0700

    add timeoutable to model

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,8 @@ class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :timeoutable
   has_many :posts, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :communities, through: :subscriptions
