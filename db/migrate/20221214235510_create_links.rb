62aae006a87ef0c079d996d787d4f3fff45304de
johnfinley <johnfinleybremerton@gmail.com>
2022-12-14 18:31:27 -0700

    migration file for links

---
+++ db/migrate/20221214235510_create_links.rb
@@ -0,0 +1,10 @@
+class CreateLinks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :slug
+      t.integer :clicked, default: 0
+      t.timestamps
+    end
+  end
+end
